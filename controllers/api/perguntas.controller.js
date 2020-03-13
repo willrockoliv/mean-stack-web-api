62fe237dc898bcaf6a1030a33d2eed9c5773f88d
William Rocha <william.r.o@hotmail.com>
2020-03-13 11:43:50 -0300

    commit inicial

---
+++ controllers/api/perguntas.controller.js
@@ -0,0 +1,58 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var perguntaService = require('services/pergunta.service');
+
+// routes
+router.post('/register', registerPergunta);
+router.get('/:_id', getCurrentPergunta);
+router.put('/:_id', updatePergunta);
+router.delete('/:_id', deletePergunta);
+
+module.exports = router;
+
+
+
+function registerPergunta(req, res) {
+    perguntaService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentPergunta(req, res) {
+    perguntaService.getById(req.params._id)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updatePergunta(req, res) {
+    perguntaService.update(req.params._id, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deletePergunta(req, res) {
+    perguntaService.delete(req.params._id)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
